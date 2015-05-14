513b3e4f606d3ff77516fe015b74e513114170cb
Thiago Machado <ti-thiago@outlook.com>
2015-05-13 21:07:41 -0300

    Primeir commit

---
+++ js/principal.js
@@ -0,0 +1,12 @@
+var banners =
+["Dolixo ao luxo!", "Reaproveitar é aproveitar!"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector(’h2#mensagem’).textContent =
+	banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 1000);
+
